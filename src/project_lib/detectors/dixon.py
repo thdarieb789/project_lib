187f3b0cd5eb8fb4943e9ebd9711730932f6314d
thdarieb789 <dariaaaaa.ssmiiile@gmail.com>
2026-06-16 11:48:45 +0300

    fix(dixon): обнаруживать выбросы с обоих концов ряда

--- src/project_lib/detectors/dixon.py
+++ src/project_lib/detectors/dixon.py
@@ -13,14 +13,19 @@ class DixonQDetector(BaseDetector):
 
         sorted_series = series.sort_values()
 
-        gap = sorted_series.iloc[1] - sorted_series.iloc[0]
+        low_gap = sorted_series.iloc[1] - sorted_series.iloc[0]
+        high_gap = sorted_series.iloc[-1] - sorted_series.iloc[-2]
         range_ = sorted_series.iloc[-1] - sorted_series.iloc[0]
 
-        Q = gap / range_ if range_ != 0 else 0
+        low_q = low_gap / range_ if range_ != 0 else 0
+        high_q = high_gap / range_ if range_ != 0 else 0
 
         anomalies = pd.Series(False, index=series.index)
 
-        if Q > self.q_threshold:
+        if low_q > self.q_threshold:
             anomalies.loc[sorted_series.index[0]] = True
+
+        if high_q > self.q_threshold:
+            anomalies.loc[sorted_series.index[-1]] = True
 
         return anomalies
