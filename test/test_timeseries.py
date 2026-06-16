d521e1b2c5faf686da102e1f671be90062f1f547
thdarieb789 <dariaaaaa.ssmiiile@gmail.com>
2026-06-16 11:49:04 +0300

    test: обновить пример использования с автоподбором и визуализацией

--- test/test_timeseries.py
+++ test/test_timeseries.py
@@ -1,6 +1,8 @@
-from project_lib import TimeSeries, IQRDetector
+from project_lib import TimeSeries, GrubbsDetector
+from project_lib.analysis.visualization import (plot_anomalies)
 
-ts = TimeSeries([10, 12, 11, 13, 999])
-detector = IQRDetector()
+ts = TimeSeries([10, 12, 11, 13, 45]) 
+detector = GrubbsDetector()
 
-print(ts.find_anomalies(detector))
+print(ts.find_anomalies_auto())
+plot_anomalies([10, 12, 11, 13, 45], detector=detector, show=True)
